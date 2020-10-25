471a8da392f27c0bfa15f8c8f3188fcbfd31d131
Colin Curtis <colinpcurtis826@ucla.edu>
2020-10-24 20:45:09 -0500

    almost complete

--- Project3/poll.cpp
+++ Project3/poll.cpp
@@ -14,6 +14,17 @@ bool isValidUppercaseStateCode(string stateCode)
             codes.find(stateCode) != string::npos);  // match found
 }
 
+// string constantLength(string forcastString) {
+//      for (int i = 0; i < forcastString.size(); i++){
+//           if (isdigit(forcastString.at(i) && isdigit(forcastString.at(i + 1))) {
+//                continue;
+//           } else if(isdigit(forcastString.at(i) &&)){
+               
+//           }
+//      }
+
+// }
+
 
 string toUppercase(string pollData) {
      string uppercase = "";
@@ -23,24 +34,7 @@ string toUppercase(string pollData) {
      return uppercase;
 }
 
-// string constantLengthForcast(string pollData) {
-//      string equalLengthPollData;
-//      for (int i = 0; i != pollData.size(); i++) {
-//           char currentPosition;
-//           char lastPosition;
-//           int lastIndex;
-//           lastIndex = i - 1;
-//           currentPosition = pollData.at(i);
-//           lastPosition = pollData.at(lastIndex);
-//           if (isdigit(currentPosition) && !isdigit(lastPosition)) {
-//                equalLengthPollData += "0";
-//           } else {
-//                equalLengthPollData += currentPosition;
-//           }
-//      }
-//      return equalLengthPollData;
-// }
-
+// adding digit to front if the state forcast is length 4; ie 5riD, then changing it to 05riD makes it easier to check in other functions
 string addDigitToFront(string pollData) {
      if (pollData != "") {
           if (isdigit(pollData.at(0)) && !isdigit(pollData.at(1))) { // add on a digit so we have constant length of 6
@@ -93,17 +87,17 @@ bool isSyntacticallyCorrect(string pollData) {
      int i = 0;
      while(i < pollData.size()) {
           
-          cout << "index " << i << endl; 
+          // cout << "index " << i << endl; 
           string substringStateForcast = "";
           substringStateForcast += pollData.at(i);
           substringStateForcast += pollData.at(i + 1);
           substringStateForcast += pollData.at(i + 2);
           substringStateForcast += pollData.at(i + 3);
           
-          cout << "forcast 1 " << substringStateForcast << endl;
+          // cout << "forcast 1 " << substringStateForcast << endl;
           if (isSyntacticallyCorrectStateForcast(substringStateForcast)) {
                i += 4;
-               cout << "stop 1" << endl;
+               // cout << "stop 1" << endl;
                continue;
           } else if (substringStateForcast.size() == pollData.size() - i) {
                return false;
@@ -114,10 +108,10 @@ bool isSyntacticallyCorrect(string pollData) {
                substringStateForcast2 += pollData.at(i + 2);
                substringStateForcast2 += pollData.at(i + 3);
                substringStateForcast2 += pollData.at(i + 4);
-               cout << "forcast 2 " << substringStateForcast2 << endl;
+               // cout << "forcast 2 " << substringStateForcast2 << endl;
                if (isSyntacticallyCorrectStateForcast(substringStateForcast2)) {
                     i += 5;
-                    cout << "stop 2" << endl;
+                    // cout << "stop 2" << endl;
                     substringStateForcast2 = "";
                     continue;
                }
@@ -128,11 +122,70 @@ bool isSyntacticallyCorrect(string pollData) {
      }
      return true;
 }
+// TODO: check 00 case
+bool checkZeroElectoralVotes(string pollData) { // return true if a state forcast predicts zero electoral votes
+     for (int i = 0; i < pollData.size(); i++) {
+          if (pollData.at(i) == '0' && !isdigit(pollData.at(i + 1))) {
+               return true;
+          } else if (pollData.at(i) == '0' && pollData.at(i + 1) == '0') {
+               return true;
+          }
+     }
+     return false;
+}
+
+
+int tallyVotes(string pollData, char party, int& voteTally) {
+
+     if (!isSyntacticallyCorrect(pollData)){
+          return 1;
+     } else if (!isalpha(party)) {
+          return 2;
+     } else if (checkZeroElectoralVotes(pollData)) {
+          return 3;
+     } else {
+          int count = 0;
+          int index = 0; // looping variable
+          while (index < pollData.size()) {
+               string electoralVotes = pollData.substr(index, 2);
+               cout << "electoral votes " << electoralVotes << endl;
+               if (isdigit(pollData.at(index) && isdigit(pollData.at(index + 1)))) {
+                    string electoralVotes = pollData.substr(index, 2);
+                    cout << "electoral votes " << electoralVotes << endl;
+               }
+               // } else if (isdigit(pollData.at(index) && !isdigit(pollData.at(index + 1)))) {
+               //      electoralVotes += pollData.at(index);
+               // }
+               // cout << "votes " << electoralVotes << endl;
+
+               // string stateForcast = "";
+
+               // stateForcast += pollData.at(index);
+               // stateForcast += pollData.at(index + 1);
+               // if (isValidUppercaseStateCode(stateForcast) && (pollData.at(index + 2) == party || pollData.at(index + 2) == toupper(party))) {
+               //      if (isdigit(pollData.at(index - 1)))
+               // }
+               break;
+          }
+          return 4;
+     }
+
+
+     
+}
 
 int main() {
      string forecast;
+     char party;
+     int voteTally;
      cout << "enter a forecast: ";
      getline(cin, forecast);
+     cout << "enter a party ";
+     cin >> party;
+     cin.ignore(1000, '\n');
 
-     cout << "forcast is: " << isSyntacticallyCorrect(forecast) << endl;
+     cout << "vote tally ";
+     cin >> voteTally;
+
+     cout << "forcast is: " << tallyVotes(forecast, party, voteTally) << endl;
 }
